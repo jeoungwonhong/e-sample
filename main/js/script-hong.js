17a00351a28e26bc139729fb13df928f72fa9c32
jeoungwonhong <alicehong1819@naver.com>
2023-04-23 20:16:39 +0900

    main-ourmodel

---
+++ main/js/script-hong.js
@@ -0,0 +1,26 @@
+$(function(){
+    $('.listbox-hong').click(function(){
+        $('#angle-hong').toggleClass('act-hong');
+        $('.s-list-hong').toggleClass('d-none-hong');
+    })
+
+
+    // 데이터 받기
+    const value_hong = document.querySelector('.select-hong span');
+    const list_hong = document.querySelectorAll('.s-list-hong li');
+
+    const handleSelect = (item) => {
+        value_hong.innerHTML = item.textContent;
+    }
+
+    // 옵션 클릭시 클릭한 옵션을 넘김
+    list_hong.forEach(option => {
+        option.addEventListener('click', () => handleSelect(option))
+    })
+
+
+    $('.libtn-hong').click(function(){
+        $('.libtn-hong').removeClass('selected-hong');
+        $(this).toggleClass('selected-hong');
+    })
+});
